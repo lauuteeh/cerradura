0f89844b1a399b926c8a9330f4ecf4d63283302d
lauuteeh <lautaro.aragones@live.com.ar>
2016-04-16 13:08:04 -0300

    Primera subida

---
+++ src/ar/edu/unlam/tallerweb1/clase3/Cerradura.java
@@ -0,0 +1,91 @@
+package ar.edu.unlam.tallerweb1.clase3;
+
+public class Cerradura {
+	
+	private Integer claveApertura;
+	public boolean estaAbierta=false;
+	private Integer fallosConsec;
+	private Integer fallosErroneos = 0;
+	private Integer AperturaLograda = 0;
+	private boolean estaBloqueada = false;
+	
+	public Cerradura(Integer clave,Integer cantidadFallos){
+		
+		claveApertura = clave;
+		fallosConsec=cantidadFallos;
+		
+	}
+	
+	
+	public boolean abrirCerradura(Integer clave){
+		
+		if(claveApertura==clave){
+			if(estaBloqueada == false){
+			AperturaLograda = AperturaLograda+1;
+			estaAbierta=true;
+			return true;
+			
+			}else{
+				fallosErroneos = fallosErroneos+1;
+			
+				if(fallosErroneos == fallosConsec){
+					estaBloqueada = true;
+				}
+			
+			return false;
+				
+			} 
+		}else{
+			
+		 return false;
+			
+		}
+	}
+	
+	public void cerrarCerradura(){
+		estaAbierta = false;
+	}	
+	
+	public boolean estaCerrado(){
+		if(estaAbierta==false){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean estaAbierto(){
+		if(estaAbierta==true){
+			return true;
+		}else{
+			return false;
+		}
+		
+
+		
+		}
+	
+	public boolean fueBloqueada(){
+		if(fallosConsec == fallosErroneos){
+			
+			return true;
+			
+		}else{
+			
+			return false;
+			
+		}
+	}
+	
+	public Integer contarAperturasExitosas(){
+		Integer valor = AperturaLograda;
+		return valor;
+	}
+
+	public Integer contarAperturasFallidas(){
+		Integer valor = fallosErroneos;
+		return valor;
+	}
+	
+	
+}
---
+++ test/ar/edu/unlam/tallerweb1/clase3/CerraduraTest.java
@@ -0,0 +1,119 @@
+package ar.edu.unlam.tallerweb1.clase3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CerraduraTest {
+
+	
+	@Test
+	public void testNoSePuedeAbrirUnaCerraduraBloqueada() {
+		Cerradura c1 = new Cerradura(123, 2);
+		c1.abrirCerradura(1);
+		c1.abrirCerradura(2);
+		c1.abrirCerradura(123);
+		Assert.assertFalse(c1.abrirCerradura(123));
+	}
+	
+	@Test
+	public void testCodigoAbreCerradura() {
+		int claveInicial = 93;
+		int falloConsecutivo =  3;
+		Cerradura cer = new Cerradura(claveInicial,falloConsecutivo);
+		
+		Assert.assertTrue(cer.abrirCerradura(93)); 
+		
+	}
+	
+	@Test
+	public void testCodigoNoAbreCerradura() {
+		int claveInicial = 93;
+		int falloConsecutivo =  3;
+		Cerradura cer = new Cerradura(claveInicial,falloConsecutivo);
+		
+		Assert.assertFalse(cer.abrirCerradura(94)); 
+		
+	}
+	
+	@Test
+	public void testCerrarCerradura(){
+		int claveInicial = 93;
+		int falloConsecutivo =  3;
+		Cerradura cer = new Cerradura(claveInicial,falloConsecutivo);
+
+		Assert.assertTrue(cer.abrirCerradura(93));
+		
+		cer.cerrarCerradura();
+		
+		Assert.assertFalse(cer.estaAbierta);
+	}
+	
+	@Test
+	public void testEstaCerrado(){
+		int claveInicial = 93;
+		int falloConsecutivo =  3;
+		Cerradura cer = new Cerradura(claveInicial,falloConsecutivo);
+				
+		Assert.assertTrue(cer.estaCerrado());
+			}
+	
+	@Test
+	public void testEstaAbierto(){
+		int claveInicial = 93;
+		int falloConsecutivo =  3;
+		Cerradura cer = new Cerradura(claveInicial,falloConsecutivo);
+				
+		Assert.assertFalse(cer.estaAbierto());
+			}
+	@Test
+	public void testFueBloqueada(){
+		int claveInicial = 93;
+		int falloConsecutivo =  3;
+		Cerradura cer = new Cerradura(claveInicial,falloConsecutivo);
+				
+		cer.abrirCerradura(94);
+		cer.abrirCerradura(94);
+		cer.abrirCerradura(94);
+		
+		Assert.assertTrue(cer.fueBloqueada());
+			}
+	
+	@Test
+	public void testConteoAperturasExitosas(){
+		
+		int claveInicial = 93;
+		int falloConsecutivo =  3;
+		Cerradura cer = new Cerradura(claveInicial,falloConsecutivo);
+		Integer valorEsperado = 2; 
+		
+		
+		cer.abrirCerradura(93);
+		cer.cerrarCerradura();
+		cer.abrirCerradura(93);
+		Integer valorObtenido = cer.contarAperturasExitosas();
+		
+		
+		Assert.assertEquals(valorEsperado,valorObtenido);	
+	}
+
+	@Test
+	public void testConteoAperturasFallidas(){
+		
+		int claveInicial = 93;
+		int falloConsecutivo =  3;
+		Cerradura cer = new Cerradura(claveInicial,falloConsecutivo);
+		Integer valorEsperado = 2; 
+		
+		
+		cer.abrirCerradura(94);
+		cer.abrirCerradura(94);
+		Integer valorObtenido = cer.contarAperturasFallidas();
+		
+		
+		Assert.assertEquals(valorEsperado,valorObtenido);	
+	}
+	
+	
+}
